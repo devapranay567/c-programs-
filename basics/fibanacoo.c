3f6a9237f1d9db4c5ddcc4d5d6870f8813f7da18
Deva Pranay Gudhe <devapranay927@gmail.com>
2025-12-06 23:16:52 +0530

    Add files via upload

---
+++ basics/fibanacoo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    int t1 = 0, t2 = 1, nextTerm;
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    printf("\n");
+    return 0;
+}
